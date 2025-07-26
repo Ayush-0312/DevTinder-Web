02095a471cf9963131a0080f45dcbd0f3f72c54b
Ayush-0312 <kumarayush3121511@gmail.com>
2025-07-26 21:08:54 +0530

    updated link

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,6 +4,7 @@ export const AVATAR = "https://avatars.githubusercontent.com/u/175679849?v=4";
 
 export const GITHUB = "https://github.com/Ayush-0312/DevTinder-Web";
 
-export const LINKEDIN = "https://www.linkedin.com/in/ayush-gupta-41484b234?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app";
+export const LINKEDIN =
+  "https://www.linkedin.com/in/ayush-gupta-41484b234?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app";
 
-export const PORTFOLIO = "https://portfolio-ayush-gupta.vercel.app/";
+export const PORTFOLIO = "https://www.theayushgupta.in/";
